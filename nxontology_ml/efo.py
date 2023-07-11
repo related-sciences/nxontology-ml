e4d15eae1d00d3701c1de8231df40c38b09b2138
Daniel Himmelstein <dhimmel@related.vc>
2023-07-11 17:07:53 -0400

    NxontologyFeaturesEfo with EFO specific NodeInfoFeaturesEfo
    uses https://github.com/related-sciences/nxontology/pull/26/commits/c503f1cf19aefbf6bc683605a850429466b3bed5

--- nxontology_ml/efo.py
+++ nxontology_ml/efo.py
@@ -1,30 +1,31 @@
 import functools
 from collections import Counter
+from typing import Any
 
-from nxontology import NXOntology
-
-from nxontology_ml.features import NodeFeatures, get_features_df
+from nxontology_ml.features import NodeInfoFeatures, NxontologyFeatures
 from nxontology_ml.utils import get_output_directory
 
 
-@functools.cache
-def get_efo_otar_slim() -> NXOntology[str]:
-    url = "https://github.com/related-sciences/nxontology-data/raw/f0e450fe3096c3b82bf531bc5125f0f7e916aad8/efo_otar_slim.json"
-    nxo: NXOntology[str] = NXOntology.read_node_link_json(url)
-    nxo.freeze()
-    return nxo
-
-
-class NodeFeaturesEfo(NodeFeatures):
+class NodeInfoFeaturesEfo(NodeInfoFeatures[str]):
     @staticmethod
     def get_curie_prefix(curie: str) -> str:
         prefix, _identifier = curie.split(":", 1)
         return prefix.lower()
 
-    def get_prefix(self) -> str:
-        curie = self.info.identifier
+    @property
+    def prefix(self) -> str:
+        curie = self.identifier
         assert isinstance(curie, str)
         return self.get_curie_prefix(curie)
+
+    @property
+    def is_gwas_trait(self) -> bool:
+        return self.data.get("gwas_trait", False)
+
+    metric_fields = NodeInfoFeatures.metric_fields + [
+        "prefix",
+        "is_gwas_trait",
+    ]
 
     efo_classification_xref_prefixes = {
         "doid": "doid",
@@ -50,26 +51,44 @@ class NodeFeaturesEfo(NodeFeatures):
 
     def get_xref_features(self) -> dict[str, int]:
         xref_counts: dict[str, int] = Counter()
-        for xref in self.info.data.get("xrefs") or []:
+        for xref in self.data.get("xrefs") or []:
             xref_counts[self.get_curie_prefix(xref)] += 1
         return {
             f"xref__{prefix_name}__count": xref_counts[prefix]
             for prefix, prefix_name in self.efo_classification_xref_prefixes.items()
         }
 
-    def get_metrics(self) -> dict[str, int | str | None]:
+    def get_metrics(self) -> dict[str, Any]:
         metrics = super().get_metrics()
-        metrics["gwas_trait"] = self.info.data.get("gwas_trait", False)
-        metrics["prefix"] = self.get_prefix()
         metrics.update(self.get_xref_features())
         return metrics
+
+
+class NxontologyFeaturesEfo(NxontologyFeatures[str]):
+    @classmethod
+    def _get_node_info_cls(cls) -> type[NodeInfoFeaturesEfo]:
+        return NodeInfoFeaturesEfo
+
+    def node_info(self, node: str) -> NodeInfoFeaturesEfo:
+        info = super().node_info(node)
+        assert isinstance(info, NodeInfoFeaturesEfo)
+        return info
+
+
+@functools.cache
+def get_efo_otar_slim() -> NxontologyFeaturesEfo:
+    url = "https://github.com/related-sciences/nxontology-data/raw/f0e450fe3096c3b82bf531bc5125f0f7e916aad8/efo_otar_slim.json"
+    nxo = NxontologyFeaturesEfo.read_node_link_json(url)
+    assert isinstance(nxo, NxontologyFeaturesEfo)
+    nxo.freeze()
+    return nxo
 
 
 def write_efo_features() -> None:
     """Generate and export features for the EFO OTAR Slim ontology."""
     nxo = get_efo_otar_slim()
     directory = get_output_directory(nxo)
-    feature_df = get_features_df(nxo, node_features_class=NodeFeaturesEfo)
+    feature_df = nxo.get_features_df()
     assert nxo.name is not None
     feature_df.to_csv(
         directory.joinpath(f"{nxo.name}_features.tsv"),
--- nxontology_ml/features.py
+++ nxontology_ml/features.py
@@ -1,3 +1,4 @@
+import logging
 from collections.abc import Iterator
 from typing import Any
 
@@ -7,44 +8,69 @@ from nxontology.node import Node as T_Node
 from nxontology.node import Node_Info
 
 
-class NodeFeatures:
-    def __init__(self, info: Node_Info[T_Node]) -> None:
-        self.info = info
+class NodeInfoFeatures(Node_Info[T_Node]):
+    @property
+    def n_parents(self) -> int:
+        return len(self.parents)
+
+    @property
+    def n_roots(self) -> int:
+        return len(self.roots)
+
+    @property
+    def n_children(self) -> int:
+        return len(self.children)
+
+    @property
+    def n_leaves(self) -> int:
+        return len(self.leaves)
+
+    metric_fields = [
+        # identifiers
+        "identifier",
+        "name",
+        # features
+        "depth",
+        "n_ancestors",
+        "n_descendants",
+        "intrinsic_ic",
+        "intrinsic_ic_scaled",
+        "intrinsic_ic_sanchez",
+        "intrinsic_ic_sanchez_scaled",
+        "n_parents",
+        "n_children",
+        "n_roots",
+        "n_leaves",
+    ]
 
     def get_metrics(self) -> dict[str, Any]:
-        metric_fields = [
-            # identifiers
-            "identifier",
-            "name",
-            # features
-            "depth",
-            "n_ancestors",
-            "n_descendants",
-            "intrinsic_ic",
-            "intrinsic_ic_scaled",
-            "intrinsic_ic_sanchez",
-            "intrinsic_ic_sanchez_scaled",
-        ]
-        metrics = {m: getattr(self.info, m) for m in metric_fields}
-        metrics["n_parents"] = len(self.info.parents)
-        metrics["n_children"] = len(self.info.children)
-        metrics["n_roots"] = len(self.info.roots)
-        metrics["n_leaves"] = len(self.info.leaves)
+        metrics = {m: getattr(self, m) for m in self.metric_fields}
         return metrics
 
 
-def get_features(
-    nxo: NXOntology[T_Node], node_features_class: type[NodeFeatures] = NodeFeatures
-) -> Iterator[dict[str, int | str | None]]:
-    """Generate features for all nodes in an nxontology."""
-    nodes = sorted(nxo.graph)
-    for node in nodes:
-        info = nxo.node_info(node)
-        yield node_features_class(info).get_metrics()
+class NxontologyFeatures(NXOntology[T_Node]):
+    @classmethod
+    def _get_node_info_cls(cls) -> type[NodeInfoFeatures[T_Node]]:
+        return NodeInfoFeatures
 
+    def node_info(self, node: T_Node) -> NodeInfoFeatures[T_Node]:
+        info = super().node_info(node)
+        assert isinstance(info, NodeInfoFeatures)
+        return info
 
-def get_features_df(
-    nxo: NXOntology[T_Node], node_features_class: type[NodeFeatures] = NodeFeatures
-) -> pd.DataFrame:
-    """Generate features for all nodes in an nxontology as a pandas DataFrame."""
-    return pd.DataFrame(get_features(nxo, node_features_class=node_features_class))
+    def get_features(self) -> Iterator[dict[str, Any]]:
+        """Generate features for all nodes in an nxontology."""
+        self.freeze()
+        nodes = sorted(self.graph)
+        for node in nodes:
+            info = self.node_info(node)
+            yield info.get_metrics()
+
+    def get_features_df(self) -> pd.DataFrame:
+        """Generate features for all nodes in an nxontology as a pandas DataFrame."""
+        feature_df = pd.DataFrame(self.get_features())
+        logging.info(
+            f"Generated {len(feature_df.columns):,} features for {len(feature_df):,} nodes."
+        )
+        logging.info(f"Features generated: {list(feature_df.columns)}")
+        return feature_df
