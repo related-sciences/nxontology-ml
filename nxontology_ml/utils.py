38c1d6f5d12309e863b895b207dbb804326d30ed
Daniel Himmelstein <dhimmel@related.vc>
2023-07-12 13:55:34 -0400

    update nxontology with NodeInfo/NodeT renames
    https://github.com/related-sciences/nxontology/commit/b68411dd7427e303c5f5e389a90185976127c78b

--- nxontology_ml/utils.py
+++ nxontology_ml/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from nxontology import NXOntology
-from nxontology.node import Node as T_Node
+from nxontology.node import NodeT
 
 
 def get_root_directory() -> Path:
@@ -9,7 +9,7 @@ def get_root_directory() -> Path:
     return Path(__file__).parent.parent
 
 
-def get_output_directory(nxo: NXOntology[T_Node]) -> Path:
+def get_output_directory(nxo: NXOntology[NodeT]) -> Path:
     """Get output directory for an nxontology, using the ontology name for the directory."""
     assert nxo.name is not None
     directory = get_root_directory().joinpath("output", nxo.name)
