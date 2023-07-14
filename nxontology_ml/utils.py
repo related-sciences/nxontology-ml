2ae8af56bd6af61bc55822b16b84b6e890e5962e
Romain Yon <yonromai@users.noreply.github.com>
2023-07-14 14:35:11 +0800

    Make source code easier to test

--- nxontology_ml/utils.py
+++ nxontology_ml/utils.py
@@ -3,15 +3,12 @@ from pathlib import Path
 from nxontology import NXOntology
 from nxontology.node import NodeT
 
-
-def get_root_directory() -> Path:
-    """Get root directory for the project."""
-    return Path(__file__).parent.parent
+ROOT_DIR: Path = Path(__file__).parent.parent
 
 
-def get_output_directory(nxo: NXOntology[NodeT]) -> Path:
+def get_output_directory(nxo: NXOntology[NodeT], parent_dir: Path = ROOT_DIR) -> Path:
     """Get output directory for an nxontology, using the ontology name for the directory."""
     assert nxo.name is not None
-    directory = get_root_directory().joinpath("output", nxo.name)
+    directory = parent_dir.joinpath("output", nxo.name)
     directory.mkdir(parents=True, exist_ok=True)
     return directory
