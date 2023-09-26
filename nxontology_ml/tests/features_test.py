e1dae9d7fd18e20c69700304043ef832ac7884ea
Romain Yon <yonromai@users.noreply.github.com>
2023-09-25 20:21:58 -0700

    Add TA feature logic

--- nxontology_ml/tests/features_test.py
+++ nxontology_ml/tests/features_test.py
@@ -1,3 +1,5 @@
+from itertools import islice
+
 from nxontology.examples import create_metal_nxo
 from sklearn.pipeline import make_pipeline
 
@@ -6,6 +8,7 @@ from nxontology_ml.features import (
     NodeInfoFeatures,
     PrepareNodeFeatures,
     SubsetsFeatures,
+    TherapeuticAreaFeatures,
 )
 from nxontology_ml.sklearn_transformer import NodeFeatures
 from nxontology_ml.tests.utils import assert_frame_equal_to
@@ -28,3 +31,25 @@ def test_subset_features() -> None:
     ]
     nf: NodeFeatures = p.transform(nodes)
     assert_frame_equal_to(nf.num_features, "subsets_features.json")
+
+
+def test_ta_features() -> None:
+    nxo = get_efo_otar_slim()
+    p = make_pipeline(PrepareNodeFeatures(nxo), TherapeuticAreaFeatures())
+    nf: NodeFeatures = p.transform(islice(sorted(nxo.graph), 5))
+    assert nf.num_features.shape == (5, 20)
+    assert nf.num_features.sparse.density == 0.08
+    feature_dict = [
+        {k: v for k, v in rec.items() if v != 0}
+        for rec in nf.num_features.to_dict(orient="records")
+    ]
+    assert feature_dict == [
+        {"TA_nervous_system_disease": 1, "TA_nutritional_or_metabolic_disease": 1},
+        {"TA_infectious_disease": 1},
+        {"TA_infectious_disease": 1, "TA_gastrointestinal_disease": 1},
+        {
+            "TA_musculoskeletal_or_connective_tissue_disease": 1,
+            "TA_respiratory_or_thoracic_disease": 1,
+        },
+        {"TA_infectious_disease": 1},
+    ]
