266a1b97a389ab564e46cde311933bb469ddc1d5
Romain Yon <yonromai@users.noreply.github.com>
2023-09-29 10:36:53 -0700

    Remove unused Model sklearn transformer

--- nxontology_ml/model.py
+++ nxontology_ml/model.py
@@ -1,12 +1,11 @@
-from typing import Any
-
 import catboost
 import numpy as np
 import pandas as pd
-from catboost import CatBoostClassifier, FeaturesData, Pool
-from sklearn.base import ClassifierMixin
+from catboost import FeaturesData
 
 from nxontology_ml.sklearn_transformer import NodeFeatures, NoFitTransformer
+
+MODEL_SEED = 42
 
 
 class CatBoostDataFormatter(NoFitTransformer[pd.DataFrame, catboost.FeaturesData]):
@@ -19,33 +18,3 @@ class CatBoostDataFormatter(NoFitTransformer[pd.DataFrame, catboost.FeaturesData
             num_feature_names=list(X.num_features.columns),
             cat_feature_names=list(X.cat_features.columns),
         )
-
-
-class CatBoostModel(ClassifierMixin):  # type: ignore[misc]
-    def __init__(self, **kwargs: Any):
-        # See CatBoostClassifier doc: https://catboost.ai/en/docs/concepts/python-reference_catboostclassifier
-        self._model: CatBoostClassifier = CatBoostClassifier(**kwargs)
-
-    def fit(
-        self, X: FeaturesData, y: list[str] | None = None, **fit_params: Any
-    ) -> "CatBoostModel":
-        assert y is not None, "Training data is required"
-        self._model.fit(X=Pool(data=X, label=list(y)), **fit_params)
-        return self
-
-    def predict(
-        self,
-        X: FeaturesData,
-        **predict_params: str,
-    ) -> np.ndarray:
-        return self._model.predict(data=Pool(data=X), **predict_params)
-
-    def get_feature_importance(self, X: FeaturesData, **gfi_params: Any) -> Any:
-        return self._model.get_feature_importance(data=Pool(data=X), **gfi_params)
-
-    def predict_proba(self, X: FeaturesData, **predict_params: Any) -> Any:
-        return self._model.predict_proba(X, **predict_params)
-
-    @property
-    def model_impl(self) -> CatBoostClassifier:
-        return self._model
--- nxontology_ml/tests/model_test.py
+++ nxontology_ml/tests/model_test.py
@@ -1,10 +1,11 @@
 import numpy as np
+from catboost import CatBoostClassifier, Pool
 from sklearn.pipeline import Pipeline, make_pipeline
 
 from nxontology_ml.data import read_training_data
 from nxontology_ml.efo import NodeXrefFeatures
 from nxontology_ml.features import NodeInfoFeatures, PrepareNodeFeatures
-from nxontology_ml.model import CatBoostDataFormatter, CatBoostModel
+from nxontology_ml.model import MODEL_SEED, CatBoostDataFormatter
 
 
 def test_e2e_model() -> None:
@@ -14,7 +15,7 @@ def test_e2e_model() -> None:
         NodeXrefFeatures(),
         CatBoostDataFormatter(),
     )
-    model = CatBoostModel(iterations=10, silent=True, random_seed=42)
+    model = CatBoostClassifier(iterations=10, silent=True, random_seed=MODEL_SEED)
     pipeline: Pipeline = make_pipeline(
         features_pipeline,
         model,
@@ -27,10 +28,10 @@ def test_e2e_model() -> None:
     assert r.shape == (1, 1)
 
     X_t = features_pipeline.transform([X[N]])
-    fi = model.get_feature_importance(X=X_t)
+    fi = model.get_feature_importance(data=Pool(data=X_t))
     assert fi.shape == (26,)
 
     fp = model.predict_proba(X=X_t)
     assert np.allclose(fp, np.array([[0.25247175, 0.22380082, 0.36932641, 0.15440102]]))
 
-    assert model.model_impl.tree_count_ == 10
+    assert model.tree_count_ == 10
